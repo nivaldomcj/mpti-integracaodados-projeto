c5d6666c4f2bcf8fd31bf363df42b6962fa48434
Nivaldo Carvalho <nivaldo_j@outlook.com>
2023-11-01 19:54:07 -0300

    reduced amount of RAM

--- start_minikube.sh
+++ start_minikube.sh
@@ -1,3 +1,3 @@
 # cpus => número de cpus a serem usados
 # memory => quantidade de ram (em MB) a serem usados
-minikube start --driver=docker --cpus=4 --memory=32768
+minikube start --driver=docker --cpus=4 --memory=8192
